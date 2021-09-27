0c928b2e212142f9b8732c634b501aed24f47aa7
NiliusJulius <nielsvhouten@gmail.com>
2021-09-27 14:56:25 +0200

    Increase performance at the cost of compile time

--- make.bat
+++ make.bat
@@ -1,27 +1,27 @@
-D:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wf--nolospre -Wl-y -Wl-w -c -o build\player_sprites.o sprites\player_sprites.c
-D:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wf--nolospre -Wl-y -Wl-w -c -o build\bullet_sprites.o sprites\bullet_sprites.c
-D:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wf--nolospre -Wl-y -Wl-w -c -o build\explosion_sprites.o sprites\explosion_sprites.c
-D:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wf--nolospre -Wl-y -Wl-w -c -o build\multiple_enemy_sprites.o sprites\multiple_enemy_sprites.c
-D:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wf--nolospre -Wl-y -Wl-w -c -o build\multiple_enemy_sprites_sec.o sprites\multiple_enemy_sprites_sec.c
+D:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wf--max-allocs-per-node50000 -Wf--opt-code-speed -Wf--nolospre -Wl-y -Wl-w -c -o build\player_sprites.o sprites\player_sprites.c
+D:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wf--max-allocs-per-node50000 -Wf--opt-code-speed -Wf--nolospre -Wl-y -Wl-w -c -o build\bullet_sprites.o sprites\bullet_sprites.c
+D:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wf--max-allocs-per-node50000 -Wf--opt-code-speed -Wf--nolospre -Wl-y -Wl-w -c -o build\explosion_sprites.o sprites\explosion_sprites.c
+D:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wf--max-allocs-per-node50000 -Wf--opt-code-speed -Wf--nolospre -Wl-y -Wl-w -c -o build\multiple_enemy_sprites.o sprites\multiple_enemy_sprites.c
+D:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wf--max-allocs-per-node50000 -Wf--opt-code-speed -Wf--nolospre -Wl-y -Wl-w -c -o build\multiple_enemy_sprites_sec.o sprites\multiple_enemy_sprites_sec.c
 
-D:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wf--nolospre -Wl-y -Wl-w -c -o build\intro_map.o maps\intro_map.c
-D:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wf--nolospre -Wl-y -Wl-w -c -o build\game_over_map.o maps\game_over_map.c
-D:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wf--nolospre -Wl-y -Wl-w -c -o build\game_background.o maps\game_background.c
+D:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wf--max-allocs-per-node50000 -Wf--opt-code-speed -Wf--nolospre -Wl-y -Wl-w -c -o build\intro_map.o maps\intro_map.c
+D:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wf--max-allocs-per-node50000 -Wf--opt-code-speed -Wf--nolospre -Wl-y -Wl-w -c -o build\game_over_map.o maps\game_over_map.c
+D:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wf--max-allocs-per-node50000 -Wf--opt-code-speed -Wf--nolospre -Wl-y -Wl-w -c -o build\game_background.o maps\game_background.c
 
-D:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wf--nolospre -Wl-y -Wl-w -c -o build\enemy_groups.o levels\enemy_groups.c
+D:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wf--max-allocs-per-node50000 -Wf--opt-code-speed -Wf--nolospre -Wl-y -Wl-w -c -o build\enemy_groups.o levels\enemy_groups.c
 
-D:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wf--nolospre -Wl-y -Wl-w -c -o build\music.o sound\music.c
-D:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wf--nolospre -Wl-y -Wl-w -c -o build\sound.o sound\sound.c
+D:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wf--max-allocs-per-node50000 -Wf--opt-code-speed -Wf--nolospre -Wl-y -Wl-w -c -o build\music.o sound\music.c
+D:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wf--max-allocs-per-node50000 -Wf--opt-code-speed -Wf--nolospre -Wl-y -Wl-w -c -o build\sound.o sound\sound.c
 
-D:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wf--nolospre -Wl-y -Wl-w -c -o build\game.o states\game.c
-D:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wf--nolospre -Wl-y -Wl-w -c -o build\intro.o states\intro.c
-D:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wf--nolospre -Wl-y -Wl-w -c -o build\game_over.o states\game_over.c
+D:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wf--max-allocs-per-node50000 -Wf--opt-code-speed -Wf--nolospre -Wl-y -Wl-w -c -o build\game.o states\game.c
+D:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wf--max-allocs-per-node50000 -Wf--opt-code-speed -Wf--nolospre -Wl-y -Wl-w -c -o build\intro.o states\intro.c
+D:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wf--max-allocs-per-node50000 -Wf--opt-code-speed -Wf--nolospre -Wl-y -Wl-w -c -o build\game_over.o states\game_over.c
 
-D:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wf--nolospre -Wl-y -Wl-w -c -o build\globals.o globals.c
+D:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wf--max-allocs-per-node50000 -Wf--opt-code-speed -Wf--nolospre -Wl-y -Wl-w -c -o build\globals.o globals.c
 
-D:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wf--nolospre -Wl-y -Wl-w -c -o build\main.o main.c
+D:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wf--max-allocs-per-node50000 -Wf--opt-code-speed -Wf--nolospre -Wl-y -Wl-w -c -o build\main.o main.c
 
-D:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wf--nolospre -Wl-y -Wl-j -Wl-w -o build\main.gb build\main.o build\player_sprites.o build\bullet_sprites.o ^
+D:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wf--max-allocs-per-node50000 -Wf--opt-code-speed -Wf--nolospre -Wl-y -Wl-j -Wl-w -o build\main.gb build\main.o build\player_sprites.o build\bullet_sprites.o ^
 build\explosion_sprites.o build\multiple_enemy_sprites.o build\multiple_enemy_sprites_sec.o build\music.o build\sound.o build\enemy_groups.o build\game.o build\globals.o build\intro.o ^
 build\intro_map.o build\game_over.o build\game_over_map.o build\game_background.o
 
